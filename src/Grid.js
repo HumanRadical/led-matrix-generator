2caf12154dfc0a0a8383c1af124de77ca504ce2f
vehikl <go@vehikl.com>
2023-06-06 16:56:52 -0400

    add styling

--- src/Grid.js
+++ src/Grid.js
@@ -1,28 +1,24 @@
 import { useState, useEffect } from 'react';
 
+const rows = 16;
+const cols = 16;
+
 const Grid = () => {
     const [grid, setGrid] = useState([]);
 
     useEffect(()=> {
-        const rows = 16;
-        const cols = 16;
         const defaultValue = 0;
 
-        const newGrid = Array.from({length: rows}, () => Array(cols).fill(defaultValue));
+        // const newGrid = Array.from({length: rows}, () => Array(cols).fill(defaultValue));
+        const newGrid = Array(rows * cols).fill(defaultValue);
         setGrid(newGrid);
     },[]);
 
     return (
-        <div>
+        <div className='grid drawGrid'>
             {
-                grid.map((row, rowIdx) =>
-                    <div key={rowIdx}>
-                        {
-                            row.map((col, colIdx) =>
-                                <div className='drawPixel' key={`${rowIdx}-${colIdx}`}>{col}</div>
-                            )
-                        }
-                    </div>
+                grid.map((pixel, pixelIndex) =>
+                    <div className='drawPixel' style={{ width: 550 / cols - 2, height: 550 / rows - 2}} key={`${pixelIndex}`}></div>
                 )
             }
         </div>
