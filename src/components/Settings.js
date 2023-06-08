be037779d2f05fe1b5eb84e8f8860d10c99be168
Easton Meth <easton.meth@gmail.com>
2023-06-08 16:42:44 -0400

    Add size settings and style

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,8 +1,26 @@
 import { useContext, useState } from "react"
 
 const Settings = () => {
+    const [cols, setCols] = useState(16)
+    const [rows, setRows] = useState(16)
+
+    const updateCols = event => {
+        setCols(event.target.value)
+        console.log(cols)
+    }
+    const updateRows = event => {
+        setRows(event.target.value)
+    }
+
     return (
-        <h1>Settings</h1>
+        <div className="settings">
+            <label className="sizeSettings">
+                Size:
+                <input className="sizeBox" onChange={updateCols} value={cols} type="number" min="1" max="99" /> 
+                <span className="sizeBy">x</span>
+                <input className="sizeBox" onChange={updateRows} value={rows} type="number" min="1" max="99" />
+            </label>
+        </div>
     )
 }
 
