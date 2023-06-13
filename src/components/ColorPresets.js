36d08f30544484b9c61928d024bcc7672b54f585
Easton Meth <easton.meth@gmail.com>
2023-06-12 23:41:22 -0400

    Select custom preset when color picker is clicked

--- src/components/ColorPresets.js
+++ src/components/ColorPresets.js
@@ -12,33 +12,33 @@ const ColorPresets = () => {
     return (
         <form className='colorPresetForm' action='#'>
             <label className='colorPresets'>
-                <input type='radio' name='colorPresets' defaultChecked={currentDrawColor === '#ff0000'}/>
+                <input type='radio' name='colorPresets' checked={currentDrawColor === '#ff0000'} readOnly/>
                 <span className='redPreset' onClick={() => setCurrentDrawColor('#ff0000')}></span>
             </label>
             <label className='colorPresets'>
-                <input type='radio' name='colorPresets' defaultChecked={currentDrawColor === '#0000ff'}/>
+                <input type='radio' name='colorPresets' checked={currentDrawColor === '#0000ff'} readOnly/>
                 <span className='bluePreset' onClick={() => setCurrentDrawColor('#0000ff')}></span>
             </label>
             <label className='colorPresets'>
-                <input type='radio' name='colorPresets' defaultChecked={currentDrawColor === '#ffff00'}/>
+                <input type='radio' name='colorPresets' checked={currentDrawColor === '#ffff00'} readOnly/>
                 <span className='yellowPreset' onClick={() => setCurrentDrawColor('#ffff00')}></span>
             </label>
             <label className='colorPresets'>
-                <input type='radio' name='colorPresets' defaultChecked={currentDrawColor ==='#00ff00'}/>
+                <input type='radio' name='colorPresets' checked={currentDrawColor ==='#00ff00'} readOnly/>
                 <span className='limePreset' onClick={() => setCurrentDrawColor('#00ff00')}></span>
             </label>
             <label className='colorPresets'>
-                <input type='radio' name='colorPresets' defaultChecked={currentDrawColor === '#ffffff'}/>
+                <input type='radio' name='colorPresets' checked={currentDrawColor === '#ffffff'} readOnly/>
                 <span className='whitePreset' onClick={() => setCurrentDrawColor('#ffffff')}></span>
             </label>
             <label className='colorPresets'>
-                <input type='radio' name='colorPresets' defaultChecked={currentDrawColor === '#000000'}/>
+                <input type='radio' name='colorPresets' checked={currentDrawColor === '#000000'} readOnly/>
                 <span className='blackPreset' onClick={() => setCurrentDrawColor('#000000')}></span>
             </label>
             <label className='colorPresets customPreset'>
-                <input type='radio' name='colorPresets' defaultChecked={currentDrawColor === 'custom'}/>
+                <input type='radio' name='colorPresets' checked={currentDrawColor === 'custom'} readOnly/>
                 <span onClick={() => setCurrentDrawColor('custom')}>Custom</span>
-                <input type='color' className='colorPicker' value={customColor} onChange={e => setCustomColor(e.target.value)} />
+                <input type='color' className='colorPicker' value={customColor} onChange={e => setCustomColor(e.target.value)} onClick={() => setCurrentDrawColor('custom')} />
             </label>
         </form>
     )
