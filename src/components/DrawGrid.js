05561da786dc7e16185d3a49cb3cb8517c16a5dc
Easton Meth <easton.meth@gmail.com>
2023-06-12 21:53:51 -0400

    Change code when grid colored in

--- src/components/DrawGrid.js
+++ src/components/DrawGrid.js
@@ -4,38 +4,50 @@ import { FramesContext } from '../context/FramesContext'
 const DrawGrid = () => {
     const {
         convertColorStringToArray,
+        convertColorArrayToString,
         cols,
         rows,
         snaked,
         frames,
+        setFrames,
         currentFrameIndex
     } = useContext(FramesContext)
 
-    const [grid, setGrid] = useState([])
+    const [grid, setGrid] = useState(convertColorStringToArray(frames[currentFrameIndex], snaked))
     const [mouseDown, setMouseDown] = useState(false)
 
     useEffect(() => {
-        setGrid(convertColorStringToArray(frames[currentFrameIndex], snaked))
-        
         document.addEventListener('mousedown', () => setMouseDown(true))
         document.addEventListener('mouseup', () => setMouseDown(false))
     }, [])
 
-    const colorInPixel = event => {
-        event.target.style.backgroundColor = "red"
+    const colorInPixel = (event, index) => {
+        event.target.style.backgroundColor = "#ff0000"
+
+        setGrid(prevGrid => {
+            const newGrid = prevGrid
+            newGrid[index] = "#ff0000"
+            return newGrid
+        })
+        
+        setFrames(prevFrames => {
+            const newFrames = prevFrames
+            newFrames[currentFrameIndex] = convertColorArrayToString(grid, snaked)
+            return newFrames
+        })
     }
 
-    const colorInPixelIfMouseDown = event => {
+    const colorInPixelIfMouseDown = (event, index) => {
         if (mouseDown) {
-            colorInPixel(event)
+            colorInPixel(event, index)
         }
     }
 
     const pixels = grid.map((pixel, pixelIndex) =>
         <div 
             className='pixel' 
-            onMouseDown={colorInPixel}
-            onMouseMove={colorInPixelIfMouseDown}
+            onMouseDown={e => colorInPixel(e, pixelIndex)}
+            onMouseMove={e => colorInPixelIfMouseDown(e, pixelIndex)}
             style={{ width: 550 / cols - 2, height: 550 / rows - 2, backgroundColor: pixel}} 
             key={`${pixelIndex}`}
         ></div>
