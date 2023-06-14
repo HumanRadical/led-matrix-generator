40a91be301b9c9531f126354c37ef936e3684fa9
Easton Meth <easton.meth@gmail.com>
2023-06-14 17:26:46 -0400

    Fix frame change rendering issue

--- src/components/CodeBox.js
+++ src/components/CodeBox.js
@@ -1,4 +1,4 @@
-import { useContext } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import { FramesContext } from '../context/FramesContext'
 
 const CodeBox = () => {
@@ -8,17 +8,24 @@ const CodeBox = () => {
         currentFrameIndex
     } = useContext(FramesContext)
 
+    const [boxValue, setBoxValue] = useState(frames[currentFrameIndex])
+
     const updateCurrentFrame = event => {
+        setBoxValue(event.target.value)
         setFrames(prevFrames => {
             const newFrames = prevFrames
             newFrames[currentFrameIndex] = event.target.value
             return newFrames
         })
-    }
+    } 
+
+    useEffect(() => {
+        setBoxValue(frames[currentFrameIndex])
+    }, [currentFrameIndex])
 
     return (
         <>
-            <textarea className='codeBox' defaultValue={frames[currentFrameIndex]} onChange={updateCurrentFrame} />
+            <textarea className='codeBox' value={boxValue} onChange={updateCurrentFrame} />
         </>
     )
 }
--- src/components/DrawGrid.js
+++ src/components/DrawGrid.js
@@ -1,4 +1,4 @@
-import { useState, useContext } from 'react'
+import { useState, useContext, useEffect } from 'react'
 import { FramesContext } from '../context/FramesContext'
 
 const DrawGrid = () => {
@@ -15,8 +15,31 @@ const DrawGrid = () => {
         currentDrawColor,
         customColor
     } = useContext(FramesContext)
-
+    
     const [grid, setGrid] = useState(convertColorStringToArray(frames[currentFrameIndex], snaked))
+    
+    const updatePixelColors = () => {
+        return grid.map((pixel, pixelIndex) => {
+            if (pixel === '<Error>') {
+                return <img src='../images/error_icon.svg' className='errorPixel' alt='' />
+            }
+            return <div 
+            className='pixel' 
+            onMouseDown={e => colorInPixel(e, pixelIndex)}
+            onMouseMove={e => colorInPixelIfMouseDown(e, pixelIndex)}
+            style={{ width: 550 / cols - 2, height: 550 / rows - 2, backgroundColor: pixel}} 
+            key={`${pixelIndex}`}
+            ></div>
+        })
+    }
+    
+    let pixels = updatePixelColors()
+
+    useEffect(() => {
+        setGrid(convertColorStringToArray(frames[currentFrameIndex], snaked))
+        pixels = updatePixelColors()
+    }, [currentFrameIndex])
+
 
     const colorInPixel = (event, index) => {
         const pixelColor = currentDrawColor === 'custom' ? customColor : currentDrawColor
@@ -41,20 +64,6 @@ const DrawGrid = () => {
             colorInPixel(event, index)
         }
     }
-
-    const pixels = grid.map((pixel, pixelIndex) => {
-        if (pixel === '<Error>') {
-            return <img src='../images/error_icon.svg' className='errorPixel' alt='' />
-        }
-        return <div 
-            className='pixel' 
-            onMouseDown={e => colorInPixel(e, pixelIndex)}
-            onMouseMove={e => colorInPixelIfMouseDown(e, pixelIndex)}
-            style={{ width: 550 / cols - 2, height: 550 / rows - 2, backgroundColor: pixel}} 
-            key={`${pixelIndex}`}
-        ></div>
-    })
-
 
     return (
         <>
