c96ba717411239f8d4ebc988343ff8dcb5c476d0
Easton Meth <easton.meth@gmail.com>
2023-06-23 11:35:10 -0400

    Create photo upload component

--- src/components/CurrentModeDisplay.js
+++ src/components/CurrentModeDisplay.js
@@ -1,11 +1,14 @@
 import CodeBox from "./CodeBox"
 import DrawGrid from "./DrawGrid"
+import PhotoUpload from "./PhotoUpload"
 
 const CurrentModeDisplay = ({currentMode}) => {
     if (currentMode === 'draw') {
         return <DrawGrid />
+    } else if (currentMode === 'code') {
+        return <CodeBox />
     }
-    return <CodeBox />
+    return <PhotoUpload />
 } 
 
 export default CurrentModeDisplay
---
+++ src/components/PhotoUpload.js
@@ -0,0 +1,7 @@
+const PhotoUpload = () => {
+    return (
+        <h1>PhotoUpload</h1>
+    )
+}
+
+export default PhotoUpload
