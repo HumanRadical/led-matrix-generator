a760a53100064ef3d45d676192fbdb81d2308464
Easton Meth <easton.meth@gmail.com>
2023-06-23 11:56:19 -0400

    Configure and style photo upload component

--- src/components/PhotoUpload.js
+++ src/components/PhotoUpload.js
@@ -1,6 +1,12 @@
 const PhotoUpload = () => {
     return (
-        <h1>PhotoUpload</h1>
+        <div className='grid photoUpload'>
+            <div className="fileUploadContainer">
+                <h3>Upload photo:</h3>
+                <label for='fileUpload'>Choose photo</label>
+                <input type='file' id='fileUpload' />
+            </div>
+        </div>
     )
 }
 
