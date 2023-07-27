aab7f1edfad47771272ce1a88f8be9a61e78e505
Easton Meth <easton.meth@gmail.com>
2023-07-27 16:35:44 -0400

    Install openai and setup ai component

--- src/components/AIPrompt.js
+++ src/components/AIPrompt.js
@@ -1,5 +1,11 @@
 import { useContext, useEffect, useState } from 'react'
 import { FramesContext } from '../context/FramesContext'
+const { Configuration, OpenAIApi } = require("openai");
+
+const configuration = new Configuration({
+  apiKey: API_KEY,
+});
+const openai = new OpenAIApi(configuration)
 
 const CodeBox = () => {
     const {
@@ -12,10 +18,28 @@ const CodeBox = () => {
 
     const [prompt, setPrompt] = useState('')
 
+    const generateImage = async () => {
+        try {
+            const response = await openai.createImage({
+                prompt: prompt,
+                n: 1,
+                size: "1024x1024",
+            })
+            const imageURL = response.data.data[0].url
+            console.log(imageURL)
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
     return (
-        <>
-            <textarea className='codeBox' />
-        </>
+        <div className='grid imageUpload'>
+            <div className='fileUploadContainer'>
+                <h3>Enter AI prompt:</h3>
+                <input type='text' onChange={e => setPrompt(e.target.value)} />
+                <button onClick={generateImage}>Submit</button>
+            </div>
+        </div>
     )
 }
 
